ddc0b922f30011de5c4e78ec6c29c5d61b6b0b41
patrickgrey <websemantic@yahoo.co.uk>
2022-09-29 10:30:45 +0100

    Tidied up and still working

--- _website-source/page-01/main.js
+++ _website-source/page-01/main.js
@@ -1,7 +1,7 @@
 import * as tiny from "@bamblehorse/tiny";
 import { local } from "/shared/js/local";
 
-console.log("tiny:", tiny("Tiny test string init!!!!"));
+console.log("tiny:", tiny("Tiny test string init???"));
 local();
 console.log("Hi from main.js");
 
