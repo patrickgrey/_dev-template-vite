6bbaa21dd6b2ec7b44aece782a72eda08cfc6ae6
patrickgrey <websemantic@yahoo.co.uk>
2022-10-05 19:50:04 +0100

    Image shortcode added

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,32 @@
 const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const Image = require("@11ty/eleventy-img");
 const CleanCSS = require("clean-css");
+const path = require("path");
+
+async function imageShortcode(src, alt, cls, sizes, widths, formats) {
+  const imagePath = path.dirname(src);
+  const urlPath = imagePath + "/";
+  const outputDir = "./website-publish" + this.page.url + imagePath + "/";
+  const imageSource = "./website-source" + this.page.url + src;
+  const sizesString = sizes || `(max-width: 2400px) 100vw, 2400px`;
+
+  let metadata = await Image(imageSource, {
+    widths: widths || [320, 640, 960, 1200, 1800, 2400],
+    formats: formats || ["avif", "webp", "jpeg"],
+    urlPath: urlPath,
+    outputDir: outputDir,
+  });
+
+  let imageAttributes = {
+    class: cls || "",
+    alt,
+    sizes: sizesString,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("copy");
@@ -7,6 +34,14 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("website-source/**/*.js");
   eleventyConfig.addPassthroughCopy("website-source/**/*.css");
   eleventyConfig.addPassthroughCopy("website-source/**/*.scss");
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.jpg`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.jpeg`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.png`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.svg`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.webp`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.avif`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.mp3`);
+  eleventyConfig.addPassthroughCopy(`website-source/**/*.pdf`);
   // eleventyConfig.addPassthroughCopy("website-source/img");
 
   // Add plugins
@@ -24,6 +59,33 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("sasstocss", function (code) {
     return (process.env.DEV_ENVIRONMENT != "dev") ? code.replace(".scss", ".css") : code;
   });
+
+  eleventyConfig.addShortcode("lmsBanner", function () {
+    let html = ``;
+    if (process.env.DEV_ENVIRONMENT === "dev") {
+      html = `<div class="ians-lms-banner">
+		<a href="/index.html">&lt;&lt; Back</a>
+	</div>`;
+    }
+    return html;
+  });
+
+  eleventyConfig.addShortcode("vimeo", function (id) {
+    const videoid = id || "692319154?h=167b8d2cc5"; //Placeholder
+    return `<div class="ians-video-16-9">
+		<iframe
+			src="https://player.vimeo.com/video/${videoid}"
+			loading="lazy"
+			width="640"
+						height="564"
+						frameborder="0"
+						allow="autoplay; fullscreen"
+						allowfullscreen
+		></iframe>
+	</div>`;
+  });
+
+  eleventyConfig.addAsyncShortcode("image", imageShortcode);
 
   return {
     // Control which files Eleventy will process
--- website-source/page-01/main.js
+++ website-source/page-01/main.js
@@ -1,12 +1,12 @@
-// import * as tiny from "@bamblehorse/tiny";
-// import { local } from "/shared/js/local";
-import { gsap } from 'gsap';
+// // import * as tiny from "@bamblehorse/tiny";
+// // import { local } from "/shared/js/local";
+// import { gsap } from 'gsap';
 
 
-// console.log("tiny:", tiny("Tiny test string init???"));
-// local();
-console.log("Hi from main.js Hiiiii");
+// // console.log("tiny:", tiny("Tiny test string init???"));
+// // local();
+// console.log("Hi from main.js Hiiiii");
 
-gsap.to(".style", { x: 200 });
+// gsap.to(".style", { x: 200 });
 
-// https://sbcode.net/threejs/module-specifiers/
+// // https://sbcode.net/threejs/module-specifiers/
