f6ad404cd47f509b8c825b57c4e2d64680344a81
patrickgrey <websemantic@yahoo.co.uk>
2022-11-05 16:29:09 +0000

    Added json to passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -43,7 +43,7 @@ async function imageShortcode(src, alt, cls, sizes, widths, formats) {
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("copy");
-  ["js", "css", "scss", "jpg", "jpeg", "png", "svg", "webp", "avif", "mp3", "pdf", "gif"].forEach((extension) => {
+  ["js", "css", "scss", "jpg", "jpeg", "png", "svg", "webp", "avif", "mp3", "pdf", "gif", "json"].forEach((extension) => {
     eleventyConfig.addPassthroughCopy(`website-source/**/*.${extension}`);
   });
 
